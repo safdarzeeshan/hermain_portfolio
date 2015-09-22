ad98dccff5b864a59e0e85d26cf41836a79781d8
Zeeshan Safdar <safdarzeeshan@gmail.com>
2015-09-22 16:11:58 -0400

    heroku updates

--- hermain_portfolio/wsgi.py
+++ hermain_portfolio/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hermain_portfolio.settings")
 
-import django.core.handlers.wsgi
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-application = django.core.handlers.wsgi.WSGIHandler()
+application = Cling(get_wsgi_application())
